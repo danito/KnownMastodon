c893a0efe1c6fe7d192d7866160079cb5ef8528d
Daniel nix <daniel.nix@gmail.com>
2017-04-05 13:58:04 +0200

    Create Auth.php

---
+++ Pages/Auth.php
@@ -0,0 +1,33 @@
+<?php
+
+    /**
+     * Plugin administration
+     */
+
+    namespace IdnoPlugins\Mastodon\Pages {
+
+        /**
+         * Default class to serve the homepage
+         */
+        class Auth extends \Idno\Common\Page
+        {
+
+            function getContent()
+            {
+                $this->gatekeeper(); // Logged-in users only
+                if ($mastodon = \Idno\Core\site()->plugins()->get('Mastodon')) {
+                    $login_url = $mastodon->getAuthURL();
+                    if (!empty($login_url)) {
+                        $this->forward($login_url); exit;
+                    }
+                }
+                $this->forward($_SERVER['HTTP_REFERER']);
+            }
+
+            function postContent() {
+                $this->getContent();
+            }
+
+        }
+
+    }

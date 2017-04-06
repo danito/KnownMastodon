6413ee1f63d505e0a4bd819a3148dd4300770fe9
Daniel nix <daniel.nix@gmail.com>
2017-04-06 09:06:03 +0200

    Create menu.tpl.php

---
+++ templates/default/admin/mastodon/menu.tpl.php
@@ -0,0 +1,1 @@
+<li <?php if ($_SERVER['REQUEST_URI'] == '/admin/importmoves/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/mastodon/">Mastodon</a></li>

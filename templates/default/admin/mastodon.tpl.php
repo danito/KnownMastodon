c23be338397920bc8b31aa6c9c6b3c027991948f
Daniel nix <daniel.nix@gmail.com>
2017-04-06 09:10:32 +0200

    Create mastodon.tpl.php

---
+++ templates/default/admin/mastodon.tpl.php
@@ -0,0 +1,12 @@
+<div class = "row">
+    <div class="col-md-10 col-md-offset-1">
+        <?= $this->draw('admin/menu') ?>
+        <h1>Syndicate to Mastodon</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <p>Your Mastodon plugin is installed. For configuration check your account settings. </p>
+    </div>
+</div>

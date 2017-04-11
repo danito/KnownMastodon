d3173e5e5afb4ceae44b81c0a0b0161e501077dd
Daniel Nix <daniel.nix@gmail.com>
2017-04-12 00:41:53 +0300

    Still wip correct autoload

--- autoload.php
+++ autoload.php
@@ -8,8 +8,8 @@
  * @license http://www.apache.org/licenses/GPL-compatibility.html GPL
  * 
  */
-define('CLASS_DIR', __DIR__);
-set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR.PATH_SEPARATOR."external");
+define('CLASS_DR', __DIR__."/external");
+set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DR);
     
 spl_autoload_register(function($name){
     //For namespaces we replace \ with / to correct the Path
